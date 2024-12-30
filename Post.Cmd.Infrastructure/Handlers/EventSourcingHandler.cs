da40d69440db1f2fdd7ae40dccdf941068472377
Nguyen.Anhduc <Nguyen.Anhduc@qualysoft-coe.com>
2024-12-30 15:13:48 +0700

    add event handler

---
+++ Post.Cmd.Infrastructure/Handlers/EventSourcingHandler.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using CQRS.Core.Domain;
+using CQRS.Core.Handlers;
+using CQRS.Core.Infrastructure;
+using Post.Cmd.Domain.Aggregates;
+using Post.Cmd.Infrastructure.Stores;
+
+namespace Post.Cmd.Infrastructure.Handlers
+{
+    public class EventSourcingHandler : IEventSourcingHandler<PostAggregate>
+    {
+        private readonly IEventStore _eventStore;
+        public EventSourcingHandler(IEventStore eventStore)
+        {
+            _eventStore = eventStore;
+        }
+        public async Task<PostAggregate> GetByIdAsync(Guid aggregateId)
+        {
+            var aggregate = new PostAggregate();
+            var events = await _eventStore.GetEventsAsync(aggregateId);
+
+            if(events == null || !events.Any()) 
+                return aggregate;
+
+            aggregate.ReplayEvent(events);
+            aggregate.Version = events.Select(item => item.Version).Max();
+
+            return aggregate;
+        }
+
+        public async Task SaveAsync(AggregateRoot aggregate)
+        {
+            await _eventStore.SaveEventsAsync(aggregate.Id, aggregate.GetUncommitedChanges(), aggregate.Version);
+            aggregate.MarkChangesAsCommited();
+        }
+    }
+}
