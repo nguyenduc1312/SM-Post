3a659920fd614dff5a99a267d140ca7c687790db
Nguyen.Anhduc <Nguyen.Anhduc@qualysoft-coe.com>
2025-01-01 14:25:43 +0700

    add repository in Post.Query.Api

---
+++ Post.Query.Domain/Repositories/ICommentRepository.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Post.Query.Domain.Entities;
+
+namespace Post.Query.Domain.Repositories
+{
+    public interface ICommentRepository
+    {
+        Task CreateAsync(CommentEntity comment);
+        Task UpdateAsync(CommentEntity comment);
+        Task DeleteAsync(CommentEntity comment);
+        Task GetByIdAsync(Guid id);
+    }
+}
---
+++ Post.Query.Domain/Repositories/IPostRepository.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Post.Query.Domain.Entities;
+
+namespace Post.Query.Domain.Repositories
+{
+    public interface IPostRepository
+    {
+        Task CreateAsync(PostEntity post);
+        Task UpdateAsync(PostEntity post);
+        Task DeleteAsync(Guid id);
+        Task<PostEntity> GetByIdAsync(Guid id);
+        Task<IEnumerable<PostEntity>> GetAllAsync();
+        Task<IEnumerable<PostEntity>> GetByAuthorAsync();
+        Task<IEnumerable<PostEntity>> GetWithLikeAsync();
+        Task<IEnumerable<PostEntity>> GetWithCommentAsync();
+    }
+}
